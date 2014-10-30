d1be78ed0e00611d458952466d8a0e33bac373dc
Iyad Kandalaft <ik@iyadk.com>
2014-10-30 11:32:26 -0400

    Skip download of IPT war if it exists in the local directory.

--- ipt-setup.sh
+++ ipt-setup.sh
@@ -6,9 +6,10 @@ sudo apt-get -q -y  install wget install tomcat7
 sudo apt-get -q -y --fix-missing install tomcat7
 
 echo Downloading IPT
-sudo wget -nv http://repository.gbif.org/content/groups/gbif/org/gbif/ipt/2.1.1/ipt-2.1.1.war
+cd /vagrant
+sudo wget -N -nv http://repository.gbif.org/content/groups/gbif/org/gbif/ipt/2.1.1/ipt-2.1.1.war
 
 echo Installing IPT war in Tomcat
-sudo mv ipt-2.1.1.war /var/lib/tomcat7/webapps/ipt.war
+sudo cp ipt*.war /var/lib/tomcat7/webapps/ROOT.war
 
 sudo service tomcat7 restart
